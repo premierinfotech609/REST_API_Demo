4533a82afaac3f3e7634f128af41a914f2ac7eb9
premierinfotech609 <premierinfotech609@gmail.com>
2020-12-21 10:31:16 -0500

    Update Database.php

--- REST_API_Demo/config/Database.php
+++ REST_API_Demo/config/Database.php
@@ -1,7 +1,7 @@
 <?php
 class Database{
 	
-	private $host  = 'localhost';
+    private $host  = 'localhost';
     private $user  = 'root';
     private $password   = "";
     private $database  = "rest_api_demo"; 
